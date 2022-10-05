94cbd5fec5eacc29e9fae79382993b2cb393cd51
akonic <ankitkachhi.ak47@gmail.com>
2022-10-06 04:36:28 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [list, setList] = useState([]);
+  const handleClick = async () => {
+    const response = await fetch(
+      "https://apicalls-c75cf-default-rtdb.firebaseio.com/Meals.json"
+    );
+    const data = await response.json();
+    const meal = [];
+    //const abs;
+    for (const i in data) {
+      meal.push({
+        id: i,
+        name: data[i].name,
+        age: data[i].age,
+      });
+    }
+    setList(meal);
+  };
+  const [age, setAge] = useState("");
+  const [name, setName] = useState("");
+  
+  const handle1 = (e) => {
+    setAge(e.target.value);
+  };
+  const handle2 = (e) => {
+    setName(e.target.value);
+  };
+  const handleClick2 = async () => {
+    fetch("https://apicalls-c75cf-default-rtdb.firebaseio.com/Meals.json", {
+      method: "POST",
+      body: JSON.stringify({
+        name: name,
+        age: age,
+      }),
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Ankit
+      <div>
+        {list.map((key) => (
+          <div>
+            <div>{key.name}</div>
+            <div>{key.age}</div>
+          </div>
+        ))}
+      </div>
+      <button onClick={handleClick}>Click here</button>
+      <div>
+        <input onChange={handle1} value={age}></input>
+        <input onChange={handle2} value={name}></input>
+        <button onClick={handleClick2}>Click to add data</button>
+      </div>
     </div>
   );
 }
